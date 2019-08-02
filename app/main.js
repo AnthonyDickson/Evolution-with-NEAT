42cc6bae2611b0d9fb71e4ccdd5dd14e6a3306cc
Anthony Dickson <eight0153@gmail.com>
2019-08-02 19:54:37 +1200

    add some colour to the scene and extend the ground

--- app/main.js
+++ app/main.js
@@ -12,6 +12,13 @@ export function main() {
         Bodies = Matter.Bodies;
     // create an engine
     const engine = Engine.create();
+
+    engine.world.bounds.min.x = -1000;
+    engine.world.bounds.min.y = 0;
+    engine.world.bounds.max.x = 1000;
+    engine.world.bounds.max.y = 600;
+    const worldWidth = Math.abs(engine.world.bounds.max.x) + Math.abs(engine.world.bounds.min.x);
+
     // create a renderer
     const render = Render.create({
         element: document.body,
@@ -20,17 +27,16 @@ export function main() {
             width: 800,
             height: 600,
             hasBounds: true,
-            showAngleIndicator: true
+            showAngleIndicator: true,
+            wireframes: false,
+            background: '#87ceeb' // skyblue
         }
     });
 
-    // create two boxes and a ground
-    const boxA = Bodies.rectangle(400, 200, 80, 80);
-    const boxB = Bodies.rectangle(450, 50, 80, 80);
-    const ground = Bodies.rectangle(400, 610, 810, 60, {isStatic: true});
-
-    // add all of the bodies to the world
-    World.add(engine.world, [boxA, boxB, ground]);
+    Render.lookAt(render, {
+        min: {x: -400, y: 0},
+        max: {x: 400, y: 600}
+    });
 
     // add mouse control
     const mouse = Mouse.create(render.canvas),
@@ -46,12 +52,19 @@ export function main() {
 
     World.add(engine.world, mouseConstraint);
 
-    engine.world.bounds.min.x = -300;
-    engine.world.bounds.min.y = -300;
-    engine.world.bounds.max.x = 1100;
-    engine.world.bounds.max.y = 900;
-
     const cameraManager = new CameraManager(mouseConstraint);
+
+    const bodyStyle = {fillStyle: '#1a1'}; // green like the Hulk
+    const groundStyle = {fillStyle: '#573b0c'}; // earthy brown
+
+    // create two boxes and a ground
+    const boxA = Bodies.rectangle(0, 200, 80, 80, {render: bodyStyle});
+    const boxB = Bodies.rectangle(0, 50, 80, 80, {render: bodyStyle});
+    const ground = Bodies.rectangle(0, 570, worldWidth, 60,
+        {isStatic: true, render: groundStyle});
+
+    // add all of the bodies to the world
+    World.add(engine.world, [boxA, boxB, ground]);
 
     // use the engine tick event to control our view
     Events.on(engine, 'beforeTick', function () {
@@ -61,7 +74,6 @@ export function main() {
     Events.on(engine, 'afterTick', function () {
         cameraManager.afterTickUpdate(engine, render, mouseConstraint);
     });
-
 
     // run the engine
     Engine.run(engine);
