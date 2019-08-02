5ec00c195d2e8846c2a24732dbf65ca44fe853af
Anthony Dickson <eight0153@gmail.com>
2019-08-02 19:20:03 +1200

    add package.json and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+require("babel-register");
+
+const config = {
+    entry: path.join(__dirname, 'app/index.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js/,
+            exclude: /(node_modules|bower_components)/,
+            use: [{
+                loader: 'babel-loader'
+            }]
+        },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Genetic Algorithms',
+            template: "app/index.html"
+        })
+    ],
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map',
+    watch: true,
+    mode: "development",
+    devServer: {
+        contentBase: './dist',
+        inline: true,
+        port: 3000
+    }
+};
+
+module.exports = config;
